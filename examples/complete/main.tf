e1cbb3048d038783ba2e02b1d01dcf3cfbc6b6b1
David B <38657416+Davidoutz@users.noreply.github.com>
2025-03-13 16:22:01 +0100

    feat!: upgrade cloudflare to V5 and add testing (#165)
    * add cloudflare tests
    
    * add cloudflare tests
    
    * remove upper limit version
    
    * add description
    
    Co-authored-by: Roland Bapst <33862357+rbapst-tamedia@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Roland Bapst <33862357+rbapst-tamedia@users.noreply.github.com>

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -26,6 +26,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.27"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5.0"
+    }
   }
 }
 
@@ -111,4 +115,44 @@ module "k8s_platform" {
 
   enable_amp = true
 
+}
+data "aws_secretsmanager_secret_version" "cloudflare" {
+  secret_id = "dai/cloudflare/tamedia/apiToken"
+}
+
+provider "cloudflare" {
+  api_token = jsondecode(data.aws_secretsmanager_secret_version.cloudflare.secret_string)["apiToken"]
+}
+
+locals {
+  zones = {
+    "kaas-example.tamedia.tech" = {
+      comment = "DAI KaaS example complete"
+    }
+  }
+}
+
+# Manage DNS sub-domaisn in cloudflare and attach them to they parent in route53
+module "cloudflare" {
+  source = "../../modules/cloudflare"
+
+  for_each = local.zones
+
+  zone_name    = module.route53_zones[each.key].route53_zone_name[each.key]
+  comment      = "Managed by KAAS examples"
+  name_servers = [for i in range(4) : module.route53_zones[each.key].route53_zone_name_servers[each.key][i]]
+  account_id   = jsondecode(data.aws_secretsmanager_secret_version.cloudflare.secret_string)["accountId"]
+}
+
+module "route53_zones" {
+  source  = "terraform-aws-modules/route53/aws//modules/zones"
+  version = "2.11.1"
+
+  for_each = local.zones
+
+  zones = {
+    (each.key) = {
+      comment = each.value.comment
+    }
+  }
 }
