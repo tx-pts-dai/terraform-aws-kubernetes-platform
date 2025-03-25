85f47b3df4462c4a5c1d373d04027668863a54cf
Samuel Wibrow <15628653+swibrow@users.noreply.github.com>
2025-03-25 13:15:18 +0100

    feat: add argocd submodule (#166)
    * feat: add argocd submodule
    
    * fix: add hacky whitespace
    
    * rework argo module
    
    * rework argo module
    
    * yaml output
    
    * feat: add argo to main module

---
+++ modules/argocd/variables.tf
@@ -0,0 +1,81 @@
+variable "create" {
+  description = "Create the ArgoCD resources"
+  type        = bool
+
+  default = true
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+
+  default = {}
+}
+
+variable "enable_hub" {
+  description = "Enable ArgoCD Hub"
+  type        = bool
+
+  default = false
+}
+
+variable "enable_spoke" {
+  description = "Enable ArgoCD Spoke"
+  type        = bool
+
+  default = false
+}
+
+variable "hub_iam_role_name" {
+  description = "IAM Role Name for ArgoCD Hub. This is referenced by the Spoke clusters"
+  type        = string
+
+  default = "argocd-controller"
+}
+
+variable "hub_iam_role_arn" {
+  description = "IAM Role ARN for ArgoCD Hub. This is required for spoke clusters"
+  type        = string
+
+  default = null
+}
+
+variable "namespace" {
+  description = "Namespace to deploy ArgoCD"
+  type        = string
+
+  default = "argocd"
+}
+
+variable "cluster_secret_labels" {
+  description = "Labels to add to the ArgoCD Cluster Secret"
+  type        = map(string)
+
+  default = {}
+}
+
+variable "cluster_secret_suffix" {
+  description = "Suffix to add to the ArgoCD Cluster Secret. This will show in the ArgoCD UI"
+  type        = string
+
+  default = ""
+}
+
+variable "helm_values" {
+  description = "Values to pass to the Helm chart"
+  type        = list(string)
+
+  default = []
+}
+
+variable "helm_set" {
+  description = "Set values to pass to the Helm chart"
+  type        = list(string)
+
+  default = []
+}
