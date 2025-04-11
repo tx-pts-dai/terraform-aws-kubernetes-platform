5c867093fb7c60a7b6b9b086637d532e3af77a54
Samuel <15628653+swibrow@users.noreply.github.com>
2025-04-11 21:49:01 +0200

    remove rsources from state

--- moved.tf
+++ moved.tf
@@ -2,3 +2,11 @@ moved {
   from = aws_security_group_rule.eks_control_plan_ingress
   to   = aws_security_group_rule.eks_control_plane_ingress
 }
+
+removed {
+  from = module.karpenter_release.helm_release.additional
+
+  lifecycle {
+    destroy = false
+  }
+}
