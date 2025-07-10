ea0e9baa04ebd1c5c5490bdd5b4fbed949fb1227
DCamma <davide.cammarata@tamedia.ch>
2025-07-10 17:49:37 +0200

    feat: update terraform-aws-modules/eks/aws module

--- main.tf
+++ main.tf
@@ -111,7 +111,7 @@ locals {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.36.0"
+  version = "20.37.1"
 
   cluster_name                    = local.stack_name
   cluster_version                 = local.k8s_version
