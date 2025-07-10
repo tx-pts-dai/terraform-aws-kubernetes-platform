f8f2ed2447c9f039e7b2055447699a7c5c3bc27e
DCamma <davide.cammarata@tamedia.ch>
2025-07-10 15:27:50 +0200

    Revert "test"
    This reverts commit 0f069741878b4f2c42b2889173559c866b7d872d.

--- modules/cloudflare/examples/main.tf
+++ modules/cloudflare/examples/main.tf
@@ -12,7 +12,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "~> 5.0"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
