45acaf44d5689491b36b44c76f6352dab1fb7e1e
DCamma <davide.cammarata@tamedia.ch>
2025-07-10 15:11:47 +0200

    remove-constraint

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.42, < 6.0.0"
+      version = ">= 5.42"
     }
     kubectl = {
       source  = "alekc/kubectl"
