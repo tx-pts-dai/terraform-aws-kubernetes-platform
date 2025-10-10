1da81876dcc25ec8761eddd0d47d8f9de353a130
Samuel Wibrow <15628653+swibrow@users.noreply.github.com>
2025-10-10 11:20:58 +0200

    feat: Support VPC endpoints in network submodule (#246)

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -22,3 +22,24 @@ output "cidr" {
   value       = var.cidr
   description = "The base CIDR block for the VPC"
 }
+
+## VPC Endpoints
+output "vpc_endpoint_ecr_api_id" {
+  description = "The ID of the ECR API VPC endpoint"
+  value       = try(aws_vpc_endpoint.ecr_api[0].id, null)
+}
+
+output "vpc_endpoint_ecr_dkr_id" {
+  description = "The ID of the ECR DKR VPC endpoint"
+  value       = try(aws_vpc_endpoint.ecr_dkr[0].id, null)
+}
+
+output "vpc_endpoint_s3_id" {
+  description = "The ID of the S3 VPC endpoint"
+  value       = try(aws_vpc_endpoint.s3[0].id, null)
+}
+
+output "vpc_endpoints_security_group_id" {
+  description = "The ID of the VPC endpoints security group"
+  value       = try(aws_security_group.vpc_endpoints[0].id, null)
+}
